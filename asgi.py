0896f7344a29cd7ab466c1bfa619eefbff0f7b86
Leeward Bound <l@lwb.co>
2022-01-16 21:12:35 -0800

    feat: channels

--- asgi.py
+++ asgi.py
@@ -7,6 +7,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fanout.conf")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fanout.settings")
 django.setup()
 application = get_default_application()
